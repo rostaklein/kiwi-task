c512acf2ed6a1b5301f37bfb0447cfe4775224fb
Rostislav Klein <rostislav.klein@dhl.com>
2018-04-19 18:50:17 +0200

    frontend init

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+            <title>T9 app</title>
+            <link rel="stylesheet" href="/_next/static/style.css" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+            <link rel="icon" type="image/png" href="/static/img/favicon_kiwi.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import "../styles/main.scss";
+export default () =>
+<div className="container">
+    <h1>This is just the start!</h1>
+    <div className="phone">
+        
+    </div>
+</div>
